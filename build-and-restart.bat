a44bd56c9fe49ebebc49b8b5d1c52c7deedd0dfc
User <user@its.me>
2023-04-21 13:15:14 +0200

    scrypt build image correction

--- build-and-restart.bat
+++ build-and-restart.bat
@@ -1,5 +1,5 @@
 docker build . -t ogloszenia
-docker stop ogloszenia
-docker rm ogloszenia
+docker stop ogloszenia || true
+docker rm ogloszenia || true
 docker run -d -p 8080:8080 --name=ogloszenia ogloszenia
 docker logs -f ogloszenia

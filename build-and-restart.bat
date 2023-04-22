de7c9f455e7098efe297d127b6f1c08187f7e370
User <user@its.me>
2023-04-22 15:57:56 +0200

    url mysql fix, and create network for docker

--- build-and-restart.bat
+++ build-and-restart.bat
@@ -1,4 +1,4 @@
 docker build . -t ogloszenia
 docker stop ogloszenia || true
 docker rm ogloszenia || true
-docker run -d -p 8080:8080 --name=ogloszenia -e SPRING_PROFILES_ACTIVE=prod ogloszenia
+docker run -d -p 8080:8080 --name=ogloszenia -e SPRING_PROFILES_ACTIVE=prod --network ogloszenia-network ogloszenia

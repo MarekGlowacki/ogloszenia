c8e7eb5cf17234456ba6ebc0df3e78c34f0ca843
User <user@its.me>
2023-04-22 15:32:36 +0200

    set prod profile variable

--- build-and-restart.bat
+++ build-and-restart.bat
@@ -1,4 +1,4 @@
 docker build . -t ogloszenia
 docker stop ogloszenia || true
 docker rm ogloszenia || true
-docker run -d -p 8080:8080 --name=ogloszenia ogloszenia
+docker run -d -p 8080:8080 --name=ogloszenia -e SPRING_PROFILES_ACTIVE=prod ogloszenia
